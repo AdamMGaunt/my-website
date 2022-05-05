a0d70016cb98c4304864b872b0033bf16049f88d
Adam Gaunt <amd.gaunt@gmail.com>
2022-05-05 14:56:18 -0600

    My first commit

--- src/App.js
+++ src/App.js
@@ -6,17 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome!</h1>
+        <p>A new version of my website is currently being created :)</p>
+        <p>And yes, it is going to be a React App.</p>
       </header>
     </div>
   );
