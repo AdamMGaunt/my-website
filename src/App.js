657728340b678acba7b38874ee8ac7b4cadb4418
Adam Gaunt <amd.gaunt@gmail.com>
2022-05-09 14:54:11 -0600

    Added SASS to the project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcome!</h1>
+        <h1>Blind Munk Studios</h1>
         <p>A new version of my website is currently being created :)</p>
         <p>And yes, it is going to be a React App.</p>
       </header>
