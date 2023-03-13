657bc3dfe835f72ee3bc199be953eee3b278c842
Adam Gaunt <amd.gaunt@gmail.com>
2023-03-13 15:41:12 -0600

    added images

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from 'react';
 // add react router to habdle navigation between pages
 // import { Route, Routes } from 'react-router-dom';
-import styled from 'styled-components';
 
 // constants
-import { MAIN_URL, PROJECTS_URL, ABOUT_URL, CONTACT_URL } from './constants/urls';
+// import { MAIN_URL, PROJECTS_URL, ABOUT_URL, CONTACT_URL } from './constants/urls';
 
 import './App.scss';
 
 // components
-import MyAppbar from './components/appbar';
-import ContentBlock from './components/block';
-import Footer from './components/footer';
-import { Section, Title } from './components/layout';
 import BaseLayout from './components/baseLayout';
 
 // Content
